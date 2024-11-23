134fc0128fcb5c78cebc676b668be92c90e89717
Faiqa-Rana <developer.faiqarana@gmail.com>
2024-11-23 15:31:21 +0500

    update the event update page

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,3 +1,4 @@
+import Container from "@/components/shared/Container";
 import { EventForm } from "@/components/shared/EventForm";
 import { getEventById } from "@/lib/actions/event.actions";
 import { auth } from "@clerk/nextjs/server";
@@ -16,20 +17,21 @@ const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
 
   return (
     <>
-      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <h3 className="wrapper h3-bold text-center sm:text-left">
-          Update Event
-        </h3>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-16">
+        <Container>
+          <h3 className="text-3xl font-bold text-center sm:text-left">
+            Update Event
+          </h3>
+          <div className="pt-6">
+            <EventForm
+              type="Update"
+              event={event}
+              eventId={event._id}
+              userId={userId}
+            />{" "}
+          </div>
+        </Container>
       </section>
-
-      <div className="wrapper my-8">
-        <EventForm
-          type="Update"
-          event={event}
-          eventId={event._id}
-          userId={userId}
-        />
-      </div>
     </>
   );
 };
