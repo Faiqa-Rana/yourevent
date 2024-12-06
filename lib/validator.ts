1a062bc198aac1806e398e3fc2fa006e38252e26
Faiqa Rana <developer.faiqarana@gmail.com>
2024-12-06 14:32:27 +0500

    apply validation

--- lib/validator.ts
+++ lib/validator.ts
@@ -5,42 +5,28 @@ export const eventFormSchema = z
     title: z.string().min(1, "Title is required"),
     categoryId: z.string().min(1, "Category is required"),
     description: z.string().min(1, "Description is required"),
-    location: z.string(),
+    location: z.string().min(1, "Location is required."),
     imageUrl: z.string().url(),
     startDateTime: z.date({ required_error: "Start date is required" }),
     endDateTime: z.date({ required_error: "End date is required" }),
-    //     price: z.string().refine(
-    //       (val) => {
-    //         const parsed = Number(val); // Convert the string to a number
-    //         return !isNaN(parsed) && parsed >= 1; // Check if it's a valid non-negative number
-    //       },
-    //       {
-    //         message: "Price must be a non-negative number", // Custom error message
-    //       }
-    //     ),
-    //     isFree: z.boolean(),
-    //     url: z.string().url("URL must be valid"),
-    //   })
+    price: z.string().refine(
+      (val) => {
+        const parsed = Number(val);
+        return !isNaN(parsed) && parsed >= 1;
+      },
+      {
+        message: "Price must be a non-negative number",
+      }
+    ),
     isFree: z.boolean(),
-    price: z
-      .string()
-      .optional()
-      .refine(
-        (val) => {
-          if (val === undefined || val === "") return true; // Allow empty when isFree is true
-          const parsed = Number(val);
-          return !isNaN(parsed) && parsed >= 1; // Validate number >= 1 when provided
-        },
-        { message: "Price must be a positive number or empty for free events" }
-      ),
     url: z.string().url("URL must be valid"),
   })
   .superRefine((data, ctx) => {
     if (data.startDateTime && data.endDateTime) {
       if (data.endDateTime <= data.startDateTime) {
         ctx.addIssue({
-          code: "custom", // Specify the issue type
-          path: ["endDateTime"], // Point to the relevant field
+          code: "custom",
+          path: ["endDateTime"],
           message: "End date must be after start date",
         });
       }
